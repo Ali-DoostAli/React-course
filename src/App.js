f9d72d5b91f02eefd22915570f4a7ce20a276de2
alidoostali <alidoosti630@gmail.com>
2022-05-28 19:03:38 +0430

    Convert App component to class base component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import Product from "./components/Product";
+import React from 'react';
+import Product from "./components/Product/Product";
 
-function App() {
-  return (
+class App extends React.Component {
+  state = {  } 
+  render() { 
+    return (
     <div className="container" id="title">
-      <h1>Shopping App</h1>
-      <Product name="React.js" price="99$" />
-      <Product name="Node.js" price="89$" />
-      <Product name="JavaScript" price="79$" />
-      <Product>
-        <p>DisCout is 20%</p>
-      </Product>
-    </div>
-  );
+          <h1>Shopping App</h1>
+           <Product name="React.js" price="99$" />
+           <Product name="Node.js" price="89$" />
+           <Product name="JavaScript" price="79$" />
+           <Product>
+             <p>DisCout is 20%</p>
+           </Product>
+        </div>  
+        );
+  }
 }
-
+ 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="container" id="title">
+//       <h1>Shopping App</h1>
+//       <Product name="React.js" price="99$" />
+//       <Product name="Node.js" price="89$" />
+//       <Product name="JavaScript" price="79$" />
+//       <Product>
+//         <p>DisCout is 20%</p>
+//       </Product>
+//     </div>
+//   );
+// }
+
+// export default App;
