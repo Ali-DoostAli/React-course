5c990c9e7c4253378af05826d701b54747ea221e
alidoostali <alidoosti630@gmail.com>
2022-05-28 20:02:30 +0430

    The concept of state and the use of the map method

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React from 'react';
 import Product from "./components/Product/Product";
 
 class App extends React.Component {
-  state = {  } 
+  state = { 
+    products: [
+      {title: "React.js" , price: "99$" , discout:"20%"},
+      {title: "Node.js" , price: "89$" , discout:"15%"},
+      {title: "JavaScript" , price: "79$" , discout:"10%"}
+    ]
+   };
   render() { 
     return (
     <div className="container" id="title">
           <h1>Shopping App</h1>
-           <Product name="React.js" price="99$" />
-           <Product name="Node.js" price="89$" />
-           <Product name="JavaScript" price="79$" />
-           <Product>
-             <p>DisCout is 20%</p>
-           </Product>
+           {this.state.products.map((product) =>{
+            return <Product
+             name={product.title}
+             price={product.price}
+             >
+               <p>Discout is: {product.discout}</p>
+             </Product>
+           })}
         </div>  
         );
   }
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,7 @@ const Product = (props) => {
     return ( <div>
         <p>product name : {props.name}</p>
         <p>product price : {props.price}</p>
-        <p>DisCout  : {props.children}</p>
+        <p>{props.children}</p>
     </div> );
 }
  
