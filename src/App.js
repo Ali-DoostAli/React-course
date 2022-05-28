f6f9724d896f5dc181ae68c87323d91a4f7efa03
alidoostali <alidoosti630@gmail.com>
2022-05-28 18:19:30 +0430

    The basic concept of props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ function App() {
   return (
     <div className="container" id="title">
       <h1>Shopping App</h1>
-      <Product />
+      <Product name="React.js" price="99$" />
+      <Product name="Node.js" price="89$" />
+      <Product name="JavaScript" price="79$" />
+      <Product>
+        <p>DisCout is 20%</p>
+      </Product>
     </div>
   );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,9 @@
-const Product = () => {
-    return ( <p>Product #1 : book</p> );
+const Product = (props) => {
+    return ( <div>
+        <p>product name : {props.name}</p>
+        <p>product price : {props.price}</p>
+        <p>DisCout  : {props.children}</p>
+    </div> );
 }
  
 export default Product;
