6b0bf2f32aa667ba4e81d41d8afec3640135d85b
alidoostali <alidoosti630@gmail.com>
2022-05-28 17:07:11 +0430

    Development of App and Product components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import Product from "./components/Product";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container" id="title">
+      <h1>Shopping App</h1>
+      <Product />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Product.js
@@ -0,0 +1,5 @@
+const Product = () => {
+    return ( <p>Product #1 : book</p> );
+}
+ 
+export default Product;
