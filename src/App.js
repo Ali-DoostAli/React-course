92a00a98d50b5e95e136d761aaf5e33d29c82979
alidoostali <alidoosti630@gmail.com>
2022-05-29 19:56:11 +0430

    Array state update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import HookCounter from "./components/HookCounter";
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import HookObject from "./components/HookObject";
+import HookArray from "./components/HookArray";
 
 const App = () => {
   return (
     <div className="App">
       {/* <HookCounter /> */}
       {/* <ClassCounter /> */}
-      <HookObject />
+      {/* <HookObject /> */}
+      <HookArray />
     </div>
   );
 };
