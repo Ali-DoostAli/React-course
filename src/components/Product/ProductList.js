aed01d1aabab0a54e15efc798cb23d0e9a2fca9b
alidoostali <alidoosti630@gmail.com>
2022-06-03 17:26:56 +0430

    conditinal rendering

--- src/components/Product/ProductList.js
+++ src/components/Product/ProductList.js
@@ -29,7 +29,7 @@ class ProductList extends Component {
     const findItem = products.find((p) => p.id === id);
     if (findItem.quantity === 1) {
       const filteredProducts = this.state.products.filter((p) => p.id !== id);
-      this.setState({ products : filteredProducts});
+      this.setState({ products: filteredProducts });
     } else {
       findItem.quantity--;
       this.setState({ products });
@@ -44,24 +44,31 @@ class ProductList extends Component {
     this.setState({ products });
   };
 
+  renderProduct = () => {
+    if (this.state.products.length === 0)
+      return <div>there is no product in cart </div>;
+
+    return this.state.products.map((product, index) => {
+      return (
+        <Product
+          product={product}
+          key={index}
+          onDelete={() => this.removeHandler(product.id)}
+          onIncrement={() => this.incrementHandler(product.id)}
+          onDecrement={() => this.decrementHandler(product.id)}
+          onChange={(e) => this.onChangeHandler(e, product.id)}
+        />
+      );
+    });
+  };
+
   render() {
     return (
       <div>
-        {this.state.products.map((product, index) => {
-          return (
-            <Product
-              product={product}
-              key={index}
-              onDelete={() => this.removeHandler(product.id)}
-              onIncrement={() => this.incrementHandler(product.id)}
-              onDecrement={() => this.decrementHandler(product.id)}
-              onChange={(e) => this.onChangeHandler(e, product.id)}
-            />
-          );
-        })}
+        {!this.state.products.length && <div>go to shopping</div>}
+        {this.renderProduct()}
       </div>
     );
   }
 }
-
 export default ProductList;
