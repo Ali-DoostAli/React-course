5a6914d72f552c71f560bbaf911de442020db6cc
alidoostali <alidoosti630@gmail.com>
2022-06-06 19:37:11 +0430

    Refactor decrement $ change

--- src/App.js
+++ src/App.js
@@ -16,43 +16,45 @@ class App extends React.Component {
     ],
   };
   removeHandler = (id) => {
-    console.log("delete", id);
     const filteredProducts = this.state.products.filter((p) => p.id !== id);
     this.setState({ products: filteredProducts });
   };
 
   incrementHandler = (id) => {
-    console.log("incremented", id);
     const index = this.state.products.findIndex((item) => item.id === id);
-    console.log(index);
+
     const product = { ...this.state.products[index] };
     product.quantity++;
-    const products = [ ...this.state.products ];
+    const products = [...this.state.products];
     products[index] = product;
     this.setState({ products });
-    // const products = [...this.state.products];
-    // const findItem = products.find((p) => p.id === id);
-    // findItem.quantity++;
-    // this.setState({ products });
   };
 
   decrementHandler = (id) => {
-    console.log("decrement", id);
-    const products = [...this.state.products];
-    const findItem = products.find((p) => p.id === id);
-    if (findItem.quantity === 1) {
+    // 1. id=ok
+    const index = this.state.products.findIndex((item) => item.id === id);
+    //  2. index = ok
+    // 3. clone the selected  index and updaate the quantity
+    const product = { ...this.state.products[index] };
+    if (product.quantity === 1) {
       const filteredProducts = this.state.products.filter((p) => p.id !== id);
       this.setState({ products: filteredProducts });
     } else {
-      findItem.quantity--;
+      // 4. updated products 
+      const products = [...this.state.products];
+      product.quantity--;
+      products[index] = product;
       this.setState({ products });
     }
   };
 
   onChangeHandler = (e, id) => {
+    const index = this.state.products.findIndex((item) => item.id === id);
+
+    const product = { ...this.state.products[index] };
+    product.title = e.target.value;
     const products = [...this.state.products];
-    const findItem = products.find((p) => p.id === id);
-    findItem.title = e.target.value;
+    products[index] = product;
     this.setState({ products });
   };
 
