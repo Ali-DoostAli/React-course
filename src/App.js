7d2f3aa0ef7f35b1287e7409b8af44217a852022
alidoostali <alidoosti630@gmail.com>
2022-06-12 19:49:52 +0430

    Convert App to Functional Component]

--- src/App.js
+++ src/App.js
@@ -7,77 +7,64 @@ import NavBar from "./components/Navbar/NavBar";
 import ProductList from "./components/Product/ProductList";
 import CountReducerTwo from "./components/Reducer/CountReducer";
 
-class App extends React.Component {
-  state = {
-    products: [
-      { title: "React.js", price: "99$", id: 1, quantity: 1 },
-      { title: "Node.js", price: "89$", id: 2, quantity: 2 },
-      { title: "JavaScript", price: "79$", id: 3, quantity: 3 },
-    ],
-    isShow: true,
-    count: 0,
-  };
-  removeHandler = (id) => {
-    const filteredProducts = this.state.products.filter((p) => p.id !== id);
-    this.setState({ products: filteredProducts });
+const App = () => {
+  const [products, setProducts] = useState([
+    { title: "React.js", price: "99$", id: 1, quantity: 1 },
+    { title: "Node.js", price: "89$", id: 2, quantity: 2 },
+    { title: "JavaScript", price: "79$", id: 3, quantity: 3 },
+  ]);
+
+  const removeHandler = (id) => {
+    const filteredProducts = products.filter((p) => p.id !== id);
+    setProducts(filteredProducts );
   };
 
-  incrementHandler = (id) => {
-    const index = this.state.products.findIndex((item) => item.id === id);
+  const incrementHandler = (id) => {
+    const index = products.findIndex((item) => item.id === id);
 
-    const product = { ...this.state.products[index] };
+    const product = { ...products[index] };
     product.quantity++;
-    const products = [...this.state.products];
-    products[index] = product;
-    this.setState({ products });
+    const updatedProducts = [...products];
+    updatedProducts[index] = product;
+    setProducts( updatedProducts );
   };
 
-  decrementHandler = (id) => {
-    // 1. id=ok
-    const index = this.state.products.findIndex((item) => item.id === id);
-    //  2. index = ok
-    // 3. clone the selected  index and updaate the quantity
-    const product = { ...this.state.products[index] };
+  const decrementHandler = (id) => {
+    const index = products.findIndex((item) => item.id === id);
+
+    const product = { ...products[index] };
     if (product.quantity === 1) {
-      const filteredProducts = this.state.products.filter((p) => p.id !== id);
-      this.setState({ products: filteredProducts });
+      const filteredProducts = products.filter((p) => p.id !== id);
+      setProducts( filteredProducts );
     } else {
-      // 4. updated products
-      const products = [...this.state.products];
+      const updatedProducts = [...products];
       product.quantity--;
-      products[index] = product;
-      this.setState({ products });
+      updatedProducts[index] = product;
+      setProducts( updatedProducts );
     }
   };
 
-  onChangeHandler = (e, id) => {
-    const index = this.state.products.findIndex((item) => item.id === id);
-    const product = { ...this.state.products[index] };
+  const onChangeHandler = (e, id) => {
+    const index = products.findIndex((item) => item.id === id);
+    const product = { ...products[index] };
     product.title = e.target.value;
-    const products = [...this.state.products];
-    products[index] = product;
-    this.setState({ products });
+    const updatedProducts = [...products];
+    updatedProducts[index] = product;
+    setProducts( updatedProducts );
   };
 
-  render() {
-    return (
-      <>
-        <CounterProvider>
-          <p>Wellcom to Context.</p>
-          <CounterOne />
-        </CounterProvider>
+  return (
+    <>
+      <NavBar totalItems={products.length} />
+      <ProductList
+        products={products}
+        onDelete={removeHandler}
+        onIncrement={incrementHandler}
+        onDecrement={decrementHandler}
+        onChange={onChangeHandler}
+      />
+    </>
+  );
+};
 
-        {/* <NavBar totalItems={this.state.products.length} />
-            <ProductList
-              products={this.state.products}
-              onDelete={this.removeHandler}
-              onIncrement={this.incrementHandler}
-              onDecrement={this.decrementHandler}
-              onChange={this.onChangeHandler}
-            /> */}
-      </>
-    );
-  }
-}
-
-export default Wrapper(App, "container");
+export default App;
